8ea9a86cf746b4013e448134fa26e3285553c0b9
Julio Angulo <jangulo@Dextras-MacBook-Pro.local>
2018-11-07 11:17:30 -0600

    solution to 1st challenge

---
+++ challenge0/challenge0.py
@@ -0,0 +1,13 @@
+"""
+solution to challenge:
+http://www.pythonchallenge.com/pc/def/0.html
+"""
+
+print(2**32)
+
+print("solution:")
+
+print("http://www.pythonchallenge.com/pc/def/0"+  str(2**32) + ".html")
+
+#solution: http://www.pythonchallenge.com/pc/def/4294967296.html
+
